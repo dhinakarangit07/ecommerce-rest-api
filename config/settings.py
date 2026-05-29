a4b68ad34b30d5061d9e63be10d7a54011b1f81a
Dhina <dhina0432@gmail.com>
2026-05-29 10:30:32 +0530

    Fixed local MySQL Docker database connection

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'NAME': 'ecommerce_db',
         'USER': 'root',
         'PASSWORD': 'root_password',   # your mysql password
-        'HOST': 'db',  
+        'HOST': '127.0.0.1',  
         'PORT': '3306',
     }
 }
