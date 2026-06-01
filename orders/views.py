34a43d74d2b620478daee8c196d65edae964837f
Dhina <dhina0432@gmail.com>
2026-06-01 17:51:01 +0530

    Updated ecommerce project

--- orders/views.py
+++ orders/views.py
@@ -9,18 +9,31 @@ from cart.models import Cart
 from .models import Order
 
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 def place_order(request):
 
-    cart_items = Cart.objects.all()
+    user = User.objects.first()
 
+    cart_items = Cart.objects.filter(user=user)
+
+    if request.method == 'GET':
+
+        total_price = sum(
+            item.product.price * item.quantity
+            for item in cart_items
+        )
+
+        return Response({
+            "cart_count": cart_items.count(),
+            "total_price": total_price
+        })
+
+    # POST
     if not cart_items.exists():
         return Response({
             "message": "Cart is Empty"
         }, status=400)
-
-    user = cart_items.first().user
 
     total_price = sum(
         item.product.price * item.quantity
